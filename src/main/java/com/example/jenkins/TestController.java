6869d6582def33265c3a9c2ae60bc5c72f2f5ebd
russell-seo <kiaofk@naver.com>
2022-03-28 16:31:43 +0900

    test

---
+++ src/main/java/com/example/jenkins/TestController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "젠킨스 성공";
+    }
+}
